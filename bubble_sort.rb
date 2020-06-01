671a106b3f7b14489fa1b40a780bf2658c14faf5
JasemDuncan <jasemvalencia@gmail.com>
2020-06-01 16:00:46 -0500

    work in bubble-sort def

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,23 +3,35 @@ def bubble_sort(arr)
   index = 0
   puts limit
   puts index
-
-  while index <= limit do
+  finish_sorting=false  
+  need_to_sort=false
+  while !finish_sorting do
     number = arr[index]
     nextNumber = arr[index + 1]
     if nextNumber && number
       if number > nextNumber
         arr[index] = nextNumber
         arr[index + 1] = number
-        index = 0
+        need_to_sort=true    
         print arr
       end
     end
-    index = index + 1
+
+    if index == limit    
+          if need_to_sort
+            index = 0
+            need_to_sort = false
+          else
+            finish_sorting = true
+          end
+    else
+      index = index + 1
+    end
   end
-  puts "result"
   print arr
+  puts " "
 end
 
-array = [4,3,78,2,0,2]
+# array = [4,3,78,2,0,2]
+array = [10,9,8,7,6,5,4,3,2,1,1,1,0,0]
 bubble_sort(array)
